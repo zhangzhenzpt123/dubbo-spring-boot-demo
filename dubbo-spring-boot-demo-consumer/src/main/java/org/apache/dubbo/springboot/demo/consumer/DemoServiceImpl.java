5ae4d91590ee9f5ee0347daae0bfd629b755fb54
zhangz <zhangz@processon.com>
2024-05-15 10:40:28 +0800

    初始化项目

---
+++ dubbo-spring-boot-demo-consumer/src/main/java/org/apache/dubbo/springboot/demo/consumer/DemoServiceImpl.java
@@ -0,0 +1,14 @@
+package org.apache.dubbo.springboot.demo.consumer;
+
+
+import org.apache.dubbo.config.annotation.DubboService;
+import org.apache.dubbo.springboot.demo.DemoService;
+
+@DubboService
+public class DemoServiceImpl implements DemoService {
+
+    @Override
+    public String sayHello(String name) {
+        return "Hello " + name;
+    }
+}
